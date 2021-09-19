5e1ac439306c957936a28b943d7331a3fdb96ed6
Lartg <roc.gerrety@gmail.com>
2021-09-19 12:54:23 -0800

    followed adding items to cart tutorial

--- scripts.js
+++ scripts.js
@@ -25,26 +25,78 @@ for(let i = 0; i < data.length; i++){
     const header = document.createElement('h2')
     const img = document.createElement('img')
     const paragraph = document.createElement('p')
+    const price = document.createElement('p')
     const button = document.createElement('button')
 
 //Get content for children from json data
-    header.id = data[i].name
+    
     header.innerText = data[i].name
     
     img.src = data[i].image
     img.width = 300
     img.height = 300
     
-    //need to split paragraph for styling
-    paragraph.innerText = data[i].desc + "\n" + "$" + data[i].price
+    paragraph.innerText = data[i].desc + "\n"  
+    
+    price.id = "price"
+    price.innerText = "$" + data[i].price
 
     button.dataset.price = data[i].price
     button.innerHTML = "Add to Cart"
+    button.id = data[i].name
 
 //Add children to the div we created, which adds our content to our HTML
     newDiv.appendChild(header)
     newDiv.appendChild(img)
     newDiv.appendChild(paragraph)
+    newDiv.appendChild(price)
     newDiv.appendChild(button)
     itemsContainer.appendChild(newDiv)
-}  
+}
+//Shopping cart tutorial
+const cart = []
+
+
+
+function addItem(name, price){
+    for(let i = 0; i < cart.length; i++){
+        if(cart[i].name === name){
+            cart[i].quantiy +=1
+            return
+        }
+    }
+    const item = {
+        name: name,
+        price: price,
+        quantity: 1
+    }
+    cart.push(item)
+}
+
+
+function showItems(){
+console.log(`You have ${getQuantity()} items in your cart`)
+//total cost
+console.log(`Total cost: ${getTotalCost()}`)
+
+    for(let i = 0; i < cart.length; i++){
+        console.log(`- ${cart[i].name} $${cart[i].price} x ${cart[i].quantity}`)
+    }
+}
+
+
+function getQuantity(){
+    let quantity = 0
+    for(let i = 0; i < cart.length; i++){
+        quantity += cart[i].quantity
+    }
+return quantity
+}
+
+function getTotalCost(){
+    let cost = 0
+    for(let i = 0; i < cart.length; i++){
+        cost += cart[i].price*cart[i].quantity
+    }
+    return cost.toFixed(2)   
+}
